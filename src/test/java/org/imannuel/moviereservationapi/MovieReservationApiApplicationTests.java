e73e7f24c2c0fc00d422c303c0daec50f7d3977c
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-12 23:58:31 +0700

    Initial commit

---
+++ src/test/java/org/imannuel/moviereservationapi/MovieReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imannuel.moviereservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
