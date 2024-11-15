dd8a72011133197e9e458861118a13a72c06c526
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-16 01:49:32 +0700

    feat: add reservation service

---
+++ src/main/java/org/imannuel/moviereservationapi/service/ReservationService.java
@@ -0,0 +1,24 @@
+package org.imannuel.moviereservationapi.service;
+
+import org.imannuel.moviereservationapi.dto.request.reservation.ReservationRequest;
+import org.imannuel.moviereservationapi.dto.response.reservation.ReservationListResponse;
+import org.imannuel.moviereservationapi.dto.response.reservation.ReservationResponse;
+import org.imannuel.moviereservationapi.entity.Reservation;
+
+public interface ReservationService {
+    void createReservation(ReservationRequest reservationRequest);
+
+    void createSeatReservation(String reservationId, String seatId);
+
+    boolean checkIsSeatAvailable(String seatId, String showtimeId);
+
+    void cancelReservation(String reservationId);
+
+    boolean checkIsReservationIsCancelable(String reservationId);
+
+    Reservation findReservationById(String reservationId);
+
+    ReservationResponse getReservationById(String reservationId);
+
+    ReservationListResponse getAllReservationByUserId();
+}
