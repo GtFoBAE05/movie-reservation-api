32c4011daa911c8fd348718a15a105b1f1002256
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-16 01:45:14 +0700

    feat: add date time parser

--- src/main/java/org/imannuel/moviereservationapi/utils/DateParse.java
+++ src/main/java/org/imannuel/moviereservationapi/utils/DateParse.java
@@ -4,13 +4,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateParse {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     public static LocalDate stringToLocalDate(String dateString) {
         try {
             return LocalDate.parse(dateString);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid date, please use yyyy-mm-dd format");
+        }
+    }
+
+    public static LocalDateTime stringToLocalDateTime(String dateTimeString) {
+        try {
+            return LocalDateTime.parse(dateTimeString, formatter);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid date time format. Please use yyyy-MM-dd HH:mm:ss format");
         }
     }
 }
