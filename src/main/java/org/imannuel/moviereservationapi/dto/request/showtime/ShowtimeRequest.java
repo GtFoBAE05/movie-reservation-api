a9fc65c04e1c43ff35b11863ae46c22343a22d63
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-16 23:51:29 +0700

    feat: add showtime validation

--- src/main/java/org/imannuel/moviereservationapi/dto/request/showtime/ShowtimeRequest.java
+++ src/main/java/org/imannuel/moviereservationapi/dto/request/showtime/ShowtimeRequest.java
@@ -1,5 +1,8 @@
 package org.imannuel.moviereservationapi.dto.request.showtime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -8,11 +11,16 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class ShowtimeRequest {
+    @NotBlank(message = "movieId is required")
     private String movieId;
 
+    @NotNull(message = "roomId is required")
     private Long roomId;
 
+    @NotBlank(message = "startDateTime is required")
     private String startDateTime;
 
+    @NotNull(message = "price is required")
+    @Positive(message = "price must be greater than zero")
     private Long price;
 }
